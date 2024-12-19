0143079a75d66da56702f96ed8469e31c901ccac
PlinioAlcantara <plinio.alcantara@dcx.ufpb.br>
2024-12-19 19:50:46 -0300

    Introduzindo o sistemas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'; // Certifique-se de importar Routes!
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent) },
+  { path: 'menu', loadComponent: () => import('./features/menu/menu.component').then(m => m.MenuComponent) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
